8279a1186100e8cb7c77899e6ce5054ec4030ddf
LibertineValues <62355000+LibertineValues@users.noreply.github.com>
2020-03-19 16:13:15 -0500

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -27,441 +27,392 @@ ideologies = [
         "name": "Marxism",
         "stats": {
             "morality": 100,
-            "dipl": 70,
-            "govt": 40,
-            "scty": 80
+            "state": 70,
+            "religious": 80
         }
     },
     {
         "name": "De Leonism",
         "stats": {
-            "econ": 100,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 80
+            "morality": 100,
+            "state": 30,
+            "religious": 80
         }
     },
     {
         "name": "Leninism",
         "stats": {
-            "econ": 100,
-            "dipl": 40,
-            "govt": 20,
-            "scty": 70
+            "morality": 100,
+            "state": 40,
+            "religious": 70
         }
     },
     {
         "name": "Stalinism/Maoism",
         "stats": {
-            "econ": 100,
-            "dipl": 20,
-            "govt": 0,
-            "scty": 60
+            "morality": 100,
+            "state": 20,
+            "religious": 60
         }
     },
     {
         "name": "Religious Communism",
         "stats": {
-            "econ": 100,
-            "dipl": 50,
-            "govt": 30,
-            "scty": 30
+            "morality": 100,
+            "state": 50,
+            "religious": 30
         }
     },
     {
         "name": "State Socialism",
         "stats": {
-            "econ": 80,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 70
+            "morality": 80,
+            "state": 30,
+            "religious": 70
         }
     },
     {
         "name": "Theocratic Socialism",
         "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 30,
-            "scty": 20
+            "morality": 80,
+            "state": 50,
+            "religious": 20
         }
     },
     {
         "name": "Religious Socialism",
         "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 70,
-            "scty": 20
+            "morality": 80,
+            "state": 50,
+            "religious": 20
         }
     },
     {
         "name": "Democratic Socialism",
         "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 50,
-            "scty": 80
+            "morality": 80,
+            "state": 50,
+            "religious": 80
         }
     },
     {
         "name": "Revolutionary Socialism",
         "stats": {
-            "econ": 80,
-            "dipl": 20,
-            "govt": 50,
-            "scty": 70
+            "morality": 80,
+            "state": 20,
+            "religious": 70
         }
     },
     {
         "name": "Libertarian Socialism",
         "stats": {
-            "econ": 80,
-            "dipl": 80,
-            "govt": 80,
-            "scty": 80
+            "morality": 80,
+            "state": 80,
+            "religious": 80
         }
     },
     {
         "name": "Anarcho-Syndicalism",
         "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 80
+            "morality": 80,
+            "state": 50,
+            "religious": 80
         }
     },
     {
         "name": "Left-Wing Populism",
         "stats": {
-            "econ": 60,
-            "dipl": 40,
-            "govt": 30,
-            "scty": 70
+            "morality": 60,
+            "state": 40,
+            "religious": 70
         }
     },
     {
         "name": "Theocratic Distributism",
         "stats": {
-            "econ": 60,
-            "dipl": 40,
-            "govt": 30,
-            "scty": 20
+            "morality": 60,
+            "state": 40,
+            "religious": 20
         }
     },
     {
         "name": "Distributism",
         "stats": {
-            "econ": 60,
-            "dipl": 50,
-            "govt": 50,
-            "scty": 20
+            "morality": 60,
+            "state": 50,
+            "religious": 20
         }
     },
     {
         "name": "Social Liberalism",
         "stats": {
-            "econ": 60,
-            "dipl": 60,
-            "govt": 60,
-            "scty": 80
+            "morality": 60,
+            "state": 60,
+            "religious": 80
         }
     },
     {
         "name": "Christian Democracy",
         "stats": {
-            "econ": 60,
-            "dipl": 60,
-            "govt": 50,
-            "scty": 30
+            "morality": 60,
+            "state": 60,
+            "religious": 30
         }
     },
     {
         "name": "Social Democracy",
         "stats": {
-            "econ": 60,
-            "dipl": 70,
-            "govt": 60,
-            "scty": 80
+            "morality": 60,
+            "state": 70,
+            "religious": 80
         }
     },
     {
         "name": "Progressivism",
         "stats": {
-            "econ": 60,
-            "dipl": 80,
-            "govt": 60,
-            "scty": 100
+            "morality": 60,
+            "state": 80,
+            "religious": 100
         }
     },
     {
         "name": "Anarcho-Mutualism",
         "stats": {
-            "econ": 60,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 70
+            "morality": 60,
+            "state": 50,
+            "religious": 70
         }
     },
     {
         "name": "National Totalitarianism",
         "stats": {
-            "econ": 50,
-            "dipl": 20,
-            "govt": 0,
-            "scty": 50
+            "morality": 50,
+            "state": 20,
+            "religious": 50
         }
     },
     {
         "name": "Global Totalitarianism",
         "stats": {
-            "econ": 50,
-            "dipl": 80,
-            "govt": 0,
-            "scty": 50
+            "morality": 50,
+            "state": 80,
+            "religious": 50
         }
     },
     {
         "name": "Technocracy",
         "stats": {
-            "econ": 60,
-            "dipl": 60,
-            "govt": 20,
-            "scty": 70
+            "morality": 60,
+            "state": 60,
+            "religious": 70
         }
     },
     {
         "name": "Centrist",
         "stats": {
-            "econ": 50,
-            "dipl": 50,
-            "govt": 50,
-            "scty": 50
+            "morality": 50,
+            "state": 50,
+            "religious": 50
         }
     },
     {
         "name": "Liberalism",
         "stats": {
-            "econ": 50,
-            "dipl": 60,
-            "govt": 60,
-            "scty": 60
+            "morality": 50,
+            "state": 60,
+            "religious": 60
         }
     },
     {
         "name": "Religious Anarchism",
         "stats": {
-            "econ": 50,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 20
+            "morality": 50,
+            "state": 50,
+            "religious": 20
         }
     },
     {
         "name": "Right-Wing Populism",
         "stats": {
-            "econ": 40,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 30
+            "morality": 40,
+            "state": 30,
+            "religious": 30
         }
     },
     {
         "name": "Moderate Conservatism",
         "stats": {
-            "econ": 40,
-            "dipl": 40,
-            "govt": 50,
-            "scty": 30
+            "morality": 40,
+            "state": 40,
+            "religious": 30
         }
     },
     {
         "name": "Reactionary",
         "stats": {
-            "econ": 40,
-            "dipl": 40,
-            "govt": 40,
-            "scty": 10
+            "morality": 40,
+            "state": 40,
+            "religious": 10
         }
     },
     {
         "name": "Social Libertarianism",
         "stats": {
-            "econ": 60,
-            "dipl": 70,
-            "govt": 80,
-            "scty": 70
+            "morality": 60,
+            "state": 70,
+            "religious": 70
         }
     },
     {
         "name": "Libertarianism",
         "stats": {
-            "econ": 40,
-            "dipl": 60,
-            "govt": 80,
-            "scty": 60
+            "morality": 40,
+            "state": 60,
+            "religious": 60
         }
     },
     {
         "name": "Anarcho-Egoism",
         "stats": {
-            "econ": 40,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 50
+            "morality": 40,
+            "state": 50,
+            "religious": 50
         }
     },
     {
         "name": "Nazism",
         "stats": {
-            "econ": 40,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 5
+            "morality": 40,
+            "state": 0,
+            "religious": 5
         }
     },
     {
         "name": "Autocracy",
         "stats": {
-            "econ": 50,
-            "dipl": 20,
-            "govt": 20,
-            "scty": 50
+            "morality": 50,
+            "state": 20,
+            "religious": 50
         }
     },
     {
         "name": "Fascism",
         "stats": {
-            "econ": 40,
-            "dipl": 20,
-            "govt": 20,
-            "scty": 20
+            "morality": 40,
+            "state": 20,
+            "religious": 20
         }
     },
     {
         "name": "Capitalist Fascism",
         "stats": {
-            "econ": 20,
-            "dipl": 20,
-            "govt": 20,
-            "scty": 20
+            "morality": 20,
+            "state": 20,
+            "religious": 20
         }
     },
     {
         "name": "Conservatism",
         "stats": {
-            "econ": 30,
-            "dipl": 40,
-            "govt": 40,
-            "scty": 20
+            "morality": 30,
+            "state": 40,
+            "religious": 20
         }
     },
     {
         "name": "Neo-Liberalism",
         "stats": {
-            "econ": 30,
-            "dipl": 30,
-            "govt": 50,
-            "scty": 60
+            "morality": 30,
+            "state": 30,
+            "religious": 60
         }
     },
     {
         "name": "Classical Liberalism",
         "stats": {
-            "econ": 30,
-            "dipl": 60,
-            "govt": 60,
-            "scty": 80
+            "morality": 30,
+            "state": 60,
+            "religious": 80
         }
     },
     {
         "name": "Authoritarian Capitalism",
         "stats": {
-            "econ": 20,
-            "dipl": 30,
-            "govt": 20,
-            "scty": 40
+            "morality": 20,
+            "state": 30,
+            "religious": 40
         }
     },
     {
         "name": "State Capitalism",
         "stats": {
-            "econ": 20,
-            "dipl": 50,
-            "govt": 30,
-            "scty": 50
+            "morality": 20,
+            "state": 50,
+            "religious": 50
         }
     },
     {
         "name": "Neo-Conservatism",
         "stats": {
-            "econ": 20,
-            "dipl": 20,
-            "govt": 40,
-            "scty": 20
+            "morality": 20,
+            "state": 20,
+            "religious": 20
         }
     },
     {
         "name": "Fundamentalism",
         "stats": {
-            "econ": 20,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 5
+            "morality": 20,
+            "state": 30,
+            "religious": 5
         }
     },
     {
         "name": "Libertarian Capitalism",
         "stats": {
-            "econ": 20,
-            "dipl": 50,
-            "govt": 80,
-            "scty": 60
+            "morality": 20,
+            "state": 50,
+            "religious": 60
         }
     },
     {
         "name": "Market Anarchism",
         "stats": {
-            "econ": 20,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 50
+            "morality": 20,
+            "state": 50,
+            "religious": 50
         }
     },
     {
         "name": "Objectivism",
         "stats": {
-            "econ": 10,
-            "dipl": 50,
-            "govt": 90,
-            "scty": 40
+            "morality": 10,
+            "state": 50,
+            "religious": 40
         }
     },
     {
         "name": "Totalitarian Capitalism",
         "stats": {
-            "econ": 0,
-            "dipl": 30,
-            "govt": 0,
-            "scty": 50
+            "morality": 0,
+            "state": 30,
+            "religious": 50
         }
     },
     {
         "name": "Ultra-Capitalism",
         "stats": {
-            "econ": 0,
-            "dipl": 40,
-            "govt": 50,
-            "scty": 50
+            "morality": 0,
+            "state": 40,
+            "religious": 50
         }
     },
     {
         "name": "Anarcho-Capitalism",
         "stats": {
-            "econ": 0,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 50
+            "morality": 0,
+            "state": 50,
+            "religious": 50
         }
     }
 ];
